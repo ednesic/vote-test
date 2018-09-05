395a02ffc7bf9d3e6c287dcf436ad7e4ab1ad02c
Eduardo Lopes Nesic De Freitas <edunesiccp2@icloud.com>
2018-09-05 15:15:45 -0300

    fixing gitignore for debug file

--- voteprocessor/main_test.go
+++ voteprocessor/main_test.go
@@ -14,7 +14,7 @@ func Test_procVote(t *testing.T) {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+	// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
